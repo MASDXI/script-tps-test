00001e230c52800e0653f02a9d2541a5a6d1fa54
MASDXI <sirawitt42@gamil.com>
2023-10-02 21:35:35 +0700

    fix: 14280 to 1428

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const DEFAULT_RPC_URL = `http://localhost:8545`;
 const DEFAULT_PRIVATE_KEY = `0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`;
-const DEFAULT_TRANSACTIONS = 14280;
+const DEFAULT_TRANSACTIONS = 1428;
 const DEFAULT_BLOCKTIME = 12;
 
 let RPC_URL = process.env.RPC_URL ? process.env.RPC_URL : DEFAULT_RPC_URL;
