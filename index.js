0b8331647b1e4df65bfa0121b31e2eac2e13a6fe
MASDXI <sirawitt42@gamil.com>
2023-10-02 19:46:42 +0700

    refactor: update etherjs version and refactor signTx

--- index.js
+++ index.js
@@ -1,64 +1,57 @@
-const { ethers } = require('ethers');
+const { ethers, Transaction } = require('ethers');
 require('dotenv').config();
 
 const DEFAULT_RPC_URL = `http://localhost:8545`;
 const DEFAULT_PRIVATE_KEY = `0x6eb3c9df7e91b6c96050a929b6e459138af6ae7934b21e073af8adc7b4e4db25`;
 const DEFAULT_TRANSACTIONS = 10000;
+const DEFAULT_BLOCKTIME = 5;
 
 let RPC_URL = process.env.RPC_URL ? process.env.RPC_URL : DEFAULT_RPC_URL;
 let PRIVATE_KEY = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : DEFAULT_PRIVATE_KEY;
 let TRANSACTIONS = process.env.TRANSACTIONS ? process.env.TRANSACTIONS : DEFAULT_TRANSACTIONS;
+let BLOCK_TIME = process.env.BLOCK_TIME ? process.env.BLOCK_TIME: DEFAULT_BLOCKTIME;
 
 const prepareTransactionPromise = async (privateKey, providerUrl, nTransactions) => {
-    const provider = new ethers.providers.JsonRpcProvider(providerUrl);
-    const gasPrice = await provider.getGasPrice();
+    const provider = new ethers.JsonRpcProvider(providerUrl);
     const wallet = new ethers.Wallet(privateKey, provider);
-    console.log("🚀 ~ file: index.js:16 ~ prepareTransactionPromise ~ wallet:", wallet.address)
-    let nonce = await wallet.getTransactionCount();
-    let noncePending = await provider.getTransactionCount(wallet.address, "pending");
-    console.log("🚀 ~ file: index.js:17 ~ prepareTransactionPromise ~ nonce:", nonce);
-    console.log("🚀 ~ file: index.js:20 ~ prepareTransactionPromise ~ noncePending:", noncePending);
+    const gasPrice = await provider.getGasPrice();
     
-    // const signingPromises = [];
+    let nonce = await wallet.getNonce();
+
+    const signingPromises = [];
     const startTime = Date.now();
 
     for (let i = 0; i < nTransactions; i++) {
-        // nonce++; // Increment nonce for each transaction
-
-        const tx = {
+        nonce++; // Increment nonce for each transaction
+        const tx = new Transaction({
             chainId: 235,
             to: "0x6f7090364d4aE2C1819693D6382b74C7D004b4B8",
-            value: ethers.utils.parseEther("1"),
-            // nonce: nonce,
+            value: ethers.parseEther("1"),
+            nonce: nonce,
             gasLimit: 21000,
-            gasPrice: gasPrice.add(1)
-        };
-
+            gasPrice: gasPrice
+        });
         // Sign the transaction
-        await wallet.sendTransaction(tx);
-
-        // Send the transaction
-        // const txResponse = await provider.sendTransaction(signedTx);
-
-        // Wait for the transaction to be mined
-        // await txResponse.wait();
-
-        console.log(`Preparing and sending transaction: ${i}`);
+        const signingPromise = await wallet.signTransaction(tx);
+        signingPromises.push(signingPromise);
     }
-
+    const signedTransactions = await Promise.all(signingPromises);
     const endTime = Date.now();
-    const durationInSeconds = (((endTime - startTime) / 1000) / (process.env.BLOCK_TIME * 1000));
+    const durationInSeconds = (((endTime - startTime) / 1000));
     console.log(`Prepare and send transaction time: ${durationInSeconds}`);
     console.log(`Transaction preparation and sending successful.`);
-
-    return;
+    return signedTransactions;
 };
+
+const sendTxs = () => {
+    // @TODO sendSignedTx
+}
 
 const benchmarkTPS = async (privateKey, providerUrl, nTransactions) => {
     const startTime = Date.now();
     await prepareTransactionPromise(privateKey, providerUrl, nTransactions);
     const endTime = Date.now();
-    const durationInSeconds = (endTime - startTime) / 1000;
+    const durationInSeconds = (((endTime - startTime) / 1000) / (BLOCK_TIME * 1000));
     const tps = nTransactions / durationInSeconds;
     console.log(`Benchmark complete.`);
     console.log(`Transaction amount: ${nTransactions}`)
