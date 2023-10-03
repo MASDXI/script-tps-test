ed49b8947bde5e8d5eda0de2a498324b7beaf3f6
MASDXI <sirawitt42@gmail.com>
2023-10-03 14:19:32 +0700

    refactor: optimization sendtx

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { JsonRpcProvider, Wallet, parseEther } = require('ethers');
+const { WebSocketProvider, Wallet, parseEther, ethers } = require('ethers');
 require('dotenv').config();
 
-const DEFAULT_RPC_URL = `http://localhost:8545`;
+const DEFAULT_RPC_URL = `ws://localhost:8546`;
 const DEFAULT_PRIVATE_KEY = `0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`;
 const DEFAULT_TRANSACTIONS = 1428;
 const DEFAULT_BLOCKTIME = 12;
@@ -12,11 +12,12 @@ let TRANSACTIONS = process.env.TRANSACTIONS ? process.env.TRANSACTIONS : DEFAULT
 let BLOCK_TIME = process.env.BLOCK_TIME ? process.env.BLOCK_TIME: DEFAULT_BLOCKTIME;
 
 const prepareTransactionPromise = async (privateKey, providerUrl) => {
-    const provider = new JsonRpcProvider(providerUrl);
+    const provider = new WebSocketProvider(providerUrl)
     const wallet = new Wallet(privateKey, provider);
     const gasPrice = (await provider.getFeeData()).gasPrice
     const chainId = (await provider.getNetwork()).chainId 
     let nonce = await provider.getTransactionCount(wallet.getAddress())
+    provider.destroy();
 
     const signingPromises = [];
     const startTime = Date.now();
@@ -45,19 +46,28 @@ const prepareTransactionPromise = async (privateKey, providerUrl) => {
 };
 
 const sendTxs = async (providerUrl, transactions) => {
-    const provider = new JsonRpcProvider(providerUrl);
-    const txPromises = [];
-    for (let i = 0; i < TRANSACTIONS; i++) {
-        const signingPromise = await provider.broadcastTransaction(transactions[i])
-        txPromises.push(signingPromise);
-    }
-    await Promise.all(txPromises);
-}
-
-const benchmarkTPS = async (privateKey, providerUrl) => {
+    const provider = new WebSocketProvider(providerUrl);
+    const batchSize = 250; // Please carefully adjust the batch size as needed
+    const numBatches = Math.ceil(transactions.length / batchSize);
     const startTime = Date.now();
-    const transactions = await prepareTransactionPromise(privateKey, providerUrl);
-    await sendTxs(providerUrl, transactions)
+    const sendBatch = async (batch) => {
+        const txPromises = batch.map((tx) => provider.broadcastTransaction(tx));
+        await Promise.all(txPromises);
+    };
+
+    const batchPromises = [];
+    for (let i = 0; i < numBatches; i++) {
+        const startIdx = i * batchSize;
+        const endIdx = Math.min(startIdx + batchSize, transactions.length);
+        const batch = transactions.slice(startIdx, endIdx);
+
+        // Push the promise for each batch into the array
+        batchPromises.push(sendBatch(batch));
+    }
+    
+    // Call sendBatches to send transactions in batches
+    await Promise.all(batchPromises);
+    provider.destroy();
     const endTime = Date.now();
     const durationInSeconds = (endTime - startTime) / 1000;
     const txs = TRANSACTIONS / (durationInSeconds);
@@ -66,6 +76,11 @@ const benchmarkTPS = async (privateKey, providerUrl) => {
     console.log(`Transaction amount: ${TRANSACTIONS}`)
     console.log(`Boardcast Transactions per second: ${txs.toFixed(3)} tx/s`);
     console.log(`Transactions per second: ${tps.toFixed(3)} tx/s`);
+}
+
+const benchmarkTPS = async (privateKey, providerUrl) => {
+    const transactions = await prepareTransactionPromise(privateKey, providerUrl);
+    await sendTxs(providerUrl, transactions)
     // @TODO get tx lenght in block to ensure tx are append in block?
 }
 
